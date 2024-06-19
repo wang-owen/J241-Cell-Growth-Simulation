1df6d80efd3ef726ec3d91b0db12fabfb26972f6
Owen Wang <owenwang0099@gmail.com>
2024-06-19 14:39:45 -0400

    added bacteria division

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import PetriDish from "./components/PetriDish";
 import Controls from "./components/Controls";
 
@@ -8,11 +8,74 @@ function App() {
     const [cells, setCells] = useState(
         Array(gridSize).fill(Array(gridSize).fill(false))
     );
+    const [timeInterval, setTimeInterval] = useState(1);
+    const [running, setRunning] = useState(false);
+    const intervalRef = useRef<number>(-1);
+
+    const startSim = () => {
+        return setInterval(() => {
+            setCells((prevCells) => {
+                const newCells = prevCells.map((row) => row.slice());
+                for (let i = 0; i < prevCells.length; i++) {
+                    for (let j = 0; j < prevCells[i].length; j++) {
+                        if (prevCells[i][j]) {
+                            let empty = [];
+                            if (j > 0 && !prevCells[i][j - 1])
+                                empty.push([i, j - 1]); // Left
+                            if (
+                                j < prevCells[i].length - 1 &&
+                                !prevCells[i][j + 1]
+                            )
+                                empty.push([i, j + 1]); // Right
+                            if (i > 0 && !prevCells[i - 1][j])
+                                empty.push([i - 1, j]); // Up
+                            if (
+                                i < prevCells.length - 1 &&
+                                !prevCells[i + 1][j]
+                            )
+                                empty.push([i + 1, j]); // Down
+
+                            if (empty.length > 0) {
+                                const [spreadI, spreadJ] =
+                                    empty[
+                                        Math.floor(Math.random() * empty.length)
+                                    ];
+                                newCells[spreadI][spreadJ] = true;
+                            }
+                        }
+                    }
+                }
+                return newCells;
+            });
+        }, timeInterval * 1000);
+    };
+
+    useEffect(() => {
+        if (running) {
+            if (Number.isNaN(timeInterval) || timeInterval < 1) {
+                setRunning(false);
+                alert("Time interval must be at least 1");
+            } else {
+                intervalRef.current = startSim();
+            }
+        } else {
+            if (intervalRef.current) {
+                clearInterval(intervalRef.current);
+            }
+        }
+    }, [running, timeInterval]);
+
     return (
         <>
             <div className="main">
                 <PetriDish cells={cells} setCells={setCells} />
-                <Controls gridSize={gridSize} setCells={setCells} />
+                <Controls
+                    gridSize={gridSize}
+                    setCells={setCells}
+                    setTimeInterval={setTimeInterval}
+                    running={running}
+                    setRunning={setRunning}
+                />
             </div>
         </>
     );
--- src/components/Controls.tsx
+++ src/components/Controls.tsx
@@ -3,25 +3,51 @@ import "./Controls.css";
 const Controls = ({
     gridSize,
     setCells,
+    setTimeInterval,
+    running,
+    setRunning,
 }: {
     gridSize: number;
     setCells: React.Dispatch<React.SetStateAction<boolean[]>[]>;
+    setTimeInterval: React.Dispatch<React.SetStateAction<number>>;
+    running: boolean;
+    setRunning: React.Dispatch<React.SetStateAction<boolean>>;
 }) => {
-    const reset = () => {
+    const reset = async () => {
         setCells(Array(gridSize).fill(Array(gridSize).fill(false)));
+        setRunning(false);
     };
+
+    document
+        .querySelector("input")
+        ?.addEventListener("keydown", function (event) {
+            if (
+                event.key === "Backspace" ||
+                event.key === "Delete" ||
+                (event.key >= "ArrowLeft" && event.key <= "ArrowRight")
+            )
+                return;
+            if ((event.key < "0" || event.key > "9") && event.preventDefault) {
+                event.preventDefault();
+            }
+        });
 
     return (
         <div id="controls">
-            <button>Start/Pause</button>
+            <button onClick={() => setRunning(!running)}>Start/Pause</button>
             <button onClick={reset}>Reset</button>
             <input
+                onChange={(event) =>
+                    setTimeInterval(parseInt(event.target.value))
+                }
                 id="interval-input"
                 type="number"
+                min={1}
                 step={0.1}
                 defaultValue={1}
                 placeholder="Time Interval (s)"
                 required
+                disabled={running}
             ></input>
         </div>
     );
